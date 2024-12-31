f58bb7022a1b4221308a762fdc7a34df07639f23
Kr1sti1an <slatkovskykristian@gmail.com>
2024-12-31 15:27:41 +0100

    commit 10
    Pridanie funkcie na kotrolovanie kolizie aby auto vybuchlo pri kolízií.

--- Unity/Assets/Skripty/Auto/AutoManazer.cs
+++ Unity/Assets/Skripty/Auto/AutoManazer.cs
@@ -13,6 +13,8 @@ public class AutoManazer : MonoBehaviour
     [SerializeField]
     MeshRenderer autoSietovyCitac;
 
+    [SerializeField]
+    VybuchManazer vybuchManazer;
 
     //Max hodnoty
     float maxOvladaciaRychlost = 2;
@@ -44,17 +46,20 @@ public class AutoManazer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(jeVybuchnuty){
+        if (jeVybuchnuty)
+        {
             return;
         }
 
         //Otočenie modelu auta pri zabáčaní
         hernyModel.transform.rotation = Quaternion.Euler(0, rb.linearVelocity.x * 5, 0);
 
-        if(autoSietovyCitac != null){
+        if (autoSietovyCitac != null)
+        {
             float nasobitelFarbyAuta = 0f;
 
-            if(vstup.y < 0){
+            if (vstup.y < 0)
+            {
                 nasobitelFarbyAuta = 4.0f;
             }
 
@@ -65,10 +70,12 @@ public class AutoManazer : MonoBehaviour
 
     }
 
-    private void FixedUpdate(){
+    private void FixedUpdate()
+    {
 
         //Je vybuchnuty
-        if(jeVybuchnuty){
+        if (jeVybuchnuty)
+        {
             //Použitie dragu
             rb.linearDamping = rb.linearVelocity.z * 0.1f;
             rb.linearDamping = Mathf.Clamp(rb.linearDamping, 1.5f, 10);
@@ -76,51 +83,61 @@ public class AutoManazer : MonoBehaviour
             //Pohyb vpred po vybuchu auta
             rb.MovePosition(Vector3.Lerp(transform.position, new Vector3(0, 0, transform.position.z), Time.deltaTime * 0.5f));
 
-            return; 
+            return;
         }
 
         //Použitie rozbehu
-        if(vstup.y > 0){
+        if (vstup.y > 0)
+        {
             Rozbeh();
         }
-        else{
+        else
+        {
             rb.linearDamping = 0.2f;
         }
 
         //Použitie brzdy
-        if(vstup.y < 0){
+        if (vstup.y < 0)
+        {
             Brzda();
         }
 
         Volant();
 
         //Nedovolime aby auto cuvalo
-        if(rb.linearVelocity.z <= 0){
+        if (rb.linearVelocity.z <= 0)
+        {
             rb.linearVelocity = Vector3.zero;
         }
     }
 
-    void Rozbeh(){
+    void Rozbeh()
+    {
         rb.linearDamping = 0;
 
         //Zotrvanie v rychlostnom limite
-        if(rb.linearVelocity.z >= maxVpredRychlost){
+        if (rb.linearVelocity.z >= maxVpredRychlost)
+        {
             return;
         }
 
         rb.AddForce(rb.transform.forward * rozbehNasobitel * vstup.y);
     }
 
-    void Brzda(){
+    void Brzda()
+    {
         //Nebrzdiť pokym nejdeme v pred
-        if(rb.linearVelocity.z <= 0){
+        if (rb.linearVelocity.z <= 0)
+        {
             return;
         }
         rb.AddForce(rb.transform.forward * brzdaNasobitel * vstup.y);
     }
 
-    void Volant(){
-        if(Mathf.Abs(vstup.x) > 0){
+    void Volant()
+    {
+        if (Mathf.Abs(vstup.x) > 0)
+        {
             //Pohnuť autom do strany
             float rychlostnyZakladnyLimit = rb.linearVelocity.z / 5.0f;
             rychlostnyZakladnyLimit = Mathf.Clamp01(rychlostnyZakladnyLimit);
@@ -137,23 +154,28 @@ public class AutoManazer : MonoBehaviour
             rb.linearVelocity = new Vector3(normalizovaneX * maxOvladaciaRychlost, 0, rb.linearVelocity.z);
 
         }
-        else{
+        else
+        {
             //Automaticke vycentrovanie auta
             rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, new Vector3(0, 0, rb.linearVelocity.z), Time.fixedDeltaTime * 3);
         }
     }
 
-    public void NastavVstup(Vector2 vstupnyVektor){
+    public void NastavVstup(Vector2 vstupnyVektor)
+    {
         vstupnyVektor.Normalize();
         vstup = vstupnyVektor;
     }
 
-    public void NastavMaxRychlost(float novaMaxRychlost){
+    public void NastavMaxRychlost(float novaMaxRychlost)
+    {
         maxVpredRychlost = novaMaxRychlost;
     }
 
-    IEnumerator SpomalCasCO(){
-        while (Time.timeScale > 0.2f){
+    IEnumerator SpomalCasCO()
+    {
+        while (Time.timeScale > 0.2f)
+        {
             Time.timeScale -= Time.deltaTime * 2;
 
             yield return null;
@@ -161,13 +183,39 @@ public class AutoManazer : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
-        while (Time.timeScale <= 1.0f){
+        while (Time.timeScale <= 1.0f)
+        {
             Time.timeScale += Time.deltaTime;
 
             yield return null;
         }
 
         Time.timeScale = 1.0f;
+    }
+
+    //Eventy
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        //AI auta vybuchnu len vtedy pokial sa stretnu s hracom alebo castou z hraca ktora vyleti
+        if (!jeHrac)
+        {
+            if (collision.transform.root.CompareTag("Untagged"))
+            {
+                return;
+            }
+            if (collision.transform.root.CompareTag("AutoAI"))
+            {
+                return;
+            }
+        }
+
+        Vector3 velocity = rb.linearVelocity;
+        vybuchManazer.Vybuch(velocity * 45);
+
+        jeVybuchnuty = true;
+
+        StartCoroutine(SpomalCasCO());
     }
 
 }

d7dec31e80aef5ea3a3f3ec51ae7f7590e8fcdd7
Kr1sti1an <slatkovskykristian@gmail.com>
2025-03-18 00:18:21 +0100

    commit 25
    upravil som vstup manazer script, nakolko pôvodný som naprogramoval len na klávesnicu, teraz auto bude možné ovládať len pomocou dát z arduina ktoré pôjdu cez script ArduinoInputManager

--- Unity/Assets/Skripty/Vstup/VstupManazer.cs
+++ Unity/Assets/Skripty/Vstup/VstupManazer.cs
@@ -1,33 +1,75 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class VstupManazer : MonoBehaviour
 {
-    [SerializeField]
-    AutoManazer autoManazer;
+    [SerializeField] private AutoManazer autoManazer;
 
-    private void Awake(){
-        if (!CompareTag("Player")){
+    public static VstupManazer Instance;
+
+    private float arduinoSteering = 0f;
+    private bool arduinoForward = false;
+    private bool arduinoBrake = false;
+
+    public bool useArduinoInput = true;
+
+    private void Awake()
+    {
+        if (!CompareTag("Player"))
+        {
             Destroy(this);
             return;
         }
+
+        // Singleton pre jednoduchý prístup z iných skriptov
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
     }
 
-    // Update is called once per frame
     void Update()
     {
         Vector2 vstup = Vector2.zero;
 
-        vstup.x = Input.GetAxis("Horizontal");
-        vstup.y = Input.GetAxis("Vertical");
+        if (useArduinoInput)
+        {
+            vstup.x = arduinoSteering;
+
+            if (arduinoForward && arduinoBrake)
+                vstup.y = 0f; // Obe tlačidlá stlačené - auto stojí
+            else if (arduinoForward)
+                vstup.y = 1f;
+            else if (arduinoBrake)
+                vstup.y = -1f;
+            else
+                vstup.y = 0f;
+        }
+        else
+        {
+            // Použitie vstupov z klávesnice
+            vstup.x = Input.GetAxis("Horizontal");
+            vstup.y = Input.GetAxis("Vertical");
+        }
 
         autoManazer.NastavVstup(vstup);
 
-        if(Input.GetKeyDown(KeyCode.R)){
+        if (Input.GetKeyDown(KeyCode.R))
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
+
+    public void NastavVstupZArduina(float steering, bool forward, bool brake)
+    {
+        arduinoSteering = steering;
+
+        // Pridanie mŕtvej zóny pre volant
+        const float deadZone = 0.1f; // 10% mŕtva zóna
+        if (Mathf.Abs(arduinoSteering) < deadZone)
+            arduinoSteering = 0f;
+
+        arduinoForward = forward;
+        arduinoBrake = brake;
+    }
 }
- 
